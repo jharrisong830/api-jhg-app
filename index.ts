7cc43813741fc7f572b0d1cf08e92d8eb327b9aa
John Graham <110198683+jharrisong830@users.noreply.github.com>
2025-04-09 13:33:40 -0400

    basic route

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+Bun.serve({
+    routes: {
+        "/hello": () => {
+            return new Response("Hello!");
+        }
+    },
+
+    fetch(req) {
+        return new Response(`Route "${req.method} ${req.url}" was not found`, {
+            status: 404
+        });
+    }
+});
