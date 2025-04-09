aefcc3a146e5cd4df78b02b5b942d87246a7b14d
John Graham <110198683+jharrisong830@users.noreply.github.com>
2025-04-09 16:00:19 -0400

    404 handler

--- api/main.py
+++ api/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import HTTPException
 
 from api.routes.index import router
 
-app = FastAPI()
+app = FastAPI(title="api.jhg.app")
 app.include_router(router)
+
+@app.exception_handler(404)
+async def http_exception_handler(request: Request, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={
+            "message": exc.detail,
+            "request": f"{request.method} {request.url.path}",
+            "status_code": exc.status_code
+        }
+    )
