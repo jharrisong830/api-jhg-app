cad88d589d78de292f1f0706443bf952dcf318c0
John Graham <110198683+jharrisong830@users.noreply.github.com>
2025-04-10 14:23:36 -0400

    env config update

--- api/config.py
+++ api/config.py
@@ -1,9 +1,10 @@
 from functools import lru_cache
+from os import environ
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class FirebaseConfig(BaseSettings):
-    firebase_admin_sdk_config: str
-    firebase_api_key: str
+    firebase_admin_sdk_config: str = environ.get("FIREBASE_ADMIN_SDK_CONFIG")
+    firebase_api_key: str = environ.get("FIREBASE_API_KEY")
 
     model_config = SettingsConfigDict(env_file=".env")
 
