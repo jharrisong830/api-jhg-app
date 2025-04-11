d721d905b1d0fe8b05a9e5dd91a5d156d9426da5
John Graham <110198683+jharrisong830@users.noreply.github.com>
2025-04-11 18:46:27 -0400

    uncomment on new branch

--- api/config.py
+++ api/config.py
@@ -1,15 +1,15 @@
-# from dataclasses import dataclass
-# from functools import lru_cache
-# from os import environ
-# from dotenv import load_dotenv
+from dataclasses import dataclass
+from functools import lru_cache
+from os import environ
+from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
 
-# @dataclass
-# class FirebaseConfig():
-#     firebase_admin_sdk_config: str = environ.get("FIREBASE_ADMIN_SDK_CONFIG")
-#     firebase_api_key: str = environ.get("FIREBASE_API_KEY")
+@dataclass
+class FirebaseConfig():
+    firebase_admin_sdk_config: str = environ.get("FIREBASE_ADMIN_SDK_CONFIG")
+    firebase_api_key: str = environ.get("FIREBASE_API_KEY")
 
-# @lru_cache
-# def get_firebase_config():
-#     return FirebaseConfig()
+@lru_cache
+def get_firebase_config():
+    return FirebaseConfig()
