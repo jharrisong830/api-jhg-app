df4807b2d9a43d6567af4993734edc3080bcc4c4
John Graham <110198683+jharrisong830@users.noreply.github.com>
2025-04-25 20:48:57 -0400

    redirect to docs from home

--- api/routes/index.py
+++ api/routes/index.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from fastapi.responses import RedirectResponse
 
 # from .plancord.index import router as plancord_router
 from api.util.projects import Project, projects
@@ -6,9 +7,9 @@ from api.util.projects import Project, projects
 router = APIRouter()
 # router.include_router(plancord_router, prefix="/plancord")
 
-@router.get("/hello")
-async def root_hello():
-    return "Hello!"
+@router.get("/", status_code=307)
+async def root_redirect():
+    return RedirectResponse(url="/docs")
 
 @router.get("/projects", response_model=list[Project])
 async def get_projects():
